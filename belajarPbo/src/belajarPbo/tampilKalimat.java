e04a581386e3aedc1fd46771cc7fed1a3a1e2750
inputsoalmuhika25-tech <inputsoalmuhika25@gmail.com>
2026-04-07 09:01:17 +0700

    Add files via upload

---
+++ belajarPbo/src/belajarPbo/tampilKalimat.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajarPbo;
+
+/**
+ *
+ * @author Labkomuhika
+ */
+public class tampilKalimat {
+    public static void main (String args [])
+    {
+        System.out.println("Hello guys Today in class XI RPL");
+    }
+}
